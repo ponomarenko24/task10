26ba255ce4ea3c621dbca2c1ed8676439223c014
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-05-04 20:18:04 +0300

    added Hive

--- lib/model.dart
+++ lib/model.dart
@@ -1,28 +1,53 @@
-class Currency {
-  final String code;
-  final String symbol;
+import 'package:hive/hive.dart';
+
+part 'model.g.dart';
+
+@HiveType(typeId: 0)
+class Currency extends HiveObject {
+  @HiveField(0)
+  String code;
+  @HiveField(1)
+  String symbol;
 
   Currency({required this.code, required this.symbol});
 }
 
-class AccountBalance {
-  final Currency currency;
+@HiveType(typeId: 1)
+class AccountBalance extends HiveObject {
+  @HiveField(0)
+  Currency currency;
+  @HiveField(1)
   double amount;
 
   AccountBalance({required this.currency, this.amount = 0.0});
 }
 
-enum TransactionType { income, expense }
+@HiveType(typeId: 2)
+enum TransactionType {
+  @HiveField(0)
+  income,
+  @HiveField(1)
+  expense,
+}
 
-class Transaction {
+@HiveType(typeId: 3)
+class Transaction extends HiveObject {
+  @HiveField(0)
   String id;
+  @HiveField(1)
   TransactionType type;
+  @HiveField(2)
   Currency currency;
+  @HiveField(3)
   double amount;
+  @HiveField(4)
   DateTime date;
+  @HiveField(5)
   String? description;
 
+  @HiveField(6)
   Currency? targetCurrency;
+  @HiveField(7)
   double? targetAmount;
 
   Transaction({
---
+++ lib/model.g.dart
@@ -0,0 +1,175 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CurrencyAdapter extends TypeAdapter<Currency> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Currency read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Currency(
+      code: fields[0] as String,
+      symbol: fields[1] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Currency obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.code)
+      ..writeByte(1)
+      ..write(obj.symbol);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CurrencyAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class AccountBalanceAdapter extends TypeAdapter<AccountBalance> {
+  @override
+  final int typeId = 1;
+
+  @override
+  AccountBalance read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return AccountBalance(
+      currency: fields[0] as Currency,
+      amount: fields[1] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, AccountBalance obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.currency)
+      ..writeByte(1)
+      ..write(obj.amount);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AccountBalanceAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TransactionAdapter extends TypeAdapter<Transaction> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Transaction read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Transaction(
+      id: fields[0] as String,
+      type: fields[1] as TransactionType,
+      currency: fields[2] as Currency,
+      amount: fields[3] as double,
+      date: fields[4] as DateTime?,
+      description: fields[5] as String?,
+      targetCurrency: fields[6] as Currency?,
+      targetAmount: fields[7] as double?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Transaction obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.type)
+      ..writeByte(2)
+      ..write(obj.currency)
+      ..writeByte(3)
+      ..write(obj.amount)
+      ..writeByte(4)
+      ..write(obj.date)
+      ..writeByte(5)
+      ..write(obj.description)
+      ..writeByte(6)
+      ..write(obj.targetCurrency)
+      ..writeByte(7)
+      ..write(obj.targetAmount);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TransactionAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TransactionTypeAdapter extends TypeAdapter<TransactionType> {
+  @override
+  final int typeId = 2;
+
+  @override
+  TransactionType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return TransactionType.income;
+      case 1:
+        return TransactionType.expense;
+      default:
+        return TransactionType.income;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, TransactionType obj) {
+    switch (obj) {
+      case TransactionType.income:
+        writer.writeByte(0);
+        break;
+      case TransactionType.expense:
+        writer.writeByte(1);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TransactionTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
